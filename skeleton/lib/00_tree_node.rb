30e8b1235ffb89d22b2f1b67a57cd9c1c4f24168
Yangchen Shen <shyangch@gmail.com>
2018-02-16 14:36:23 -0800

    Finish PolyTreeNode Class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
   def initialize(value, parent = nil, children = [])
     @value = value
@@ -30,5 +32,32 @@ class PolyTreeNode
   def remove_child(child_node)
     raise "Node is not a child" unless self.children.include?(child_node)
     child_node.parent=(nil)
+  end
+
+  def dfs(target_value)
+    return self if value == target_value
+    return nil if self.children.empty?
+    stack = []
+    stack.concat(self.children.reverse)
+
+    until stack.empty?
+      child = stack.pop
+      result = child.dfs(target_value)
+      return result unless result.nil?
+    end
+  end
+
+  def bfs(target_value)
+    queue = []
+    queue << self
+
+    until queue.empty?
+      cur = queue.shift
+      if cur.value == target_value
+        return cur
+      end
+
+      queue += cur.children
+    end
   end
 end
