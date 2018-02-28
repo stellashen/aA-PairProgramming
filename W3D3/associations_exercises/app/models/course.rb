d8d95a9cb04a13efe90d4e8056e91713f46b7b9c
Yangchen Shen <shyangch@gmail.com>
2018-02-28 14:24:15 -0800

    Complete association exercise

--- W3D3/associations_exercises/app/models/course.rb
+++ W3D3/associations_exercises/app/models/course.rb
@@ -1,2 +1,21 @@
 class Course < ApplicationRecord
+
+  has_many :enrollments,
+    class_name: :Enrollment,
+    foreign_key: :course_id,
+    primary_key: :id
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
+
+  has_one :prerequisite,
+    class_name: :Course,
+    primary_key: :prereq_id,
+    foreign_key: :id
+
+  has_one :instructor,
+    class_name: :User,
+    foreign_key: :id,
+    primary_key: :instructor_id
 end
