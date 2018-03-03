ec981a8bbd9e77542c7dacf11ed395a08cd19b5e
Yangchen Shen <shyangch@gmail.com>
2018-03-02 16:18:35 -0800

    Implement ::finalize! for 01_sql_object

--- W3D5/active_record_lite/lib/01_sql_object.rb
+++ W3D5/active_record_lite/lib/01_sql_object.rb
@@ -5,18 +5,59 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns unless @columns.nil?
+
+    result = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+    SQL
+
+    # convert string to symbol
+    # save columns to instance variable, so next time we get @columns directly
+    @columns = result.first.map { |col| col.to_sym }
   end
 
   def self.finalize!
+    cols = self.columns
+
+    cols.each do |col|
+      define_method("#{col}") do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |val|
+        self.attributes[col] = val
+      end
+    end
+
+    #
+    # cols.each do |col|
+    #   p @attributes
+    #
+    #   val = DBConnection.execute(<<-SQL)
+    #     SELECT
+    #       "#{col}"
+    #     FROM
+    #       "#{self.class.table_name}"
+    #   SQL
+    #
+    #   @attributes[col] = val
+    # end
+    # @attributes
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    if @table_name.nil? # If table name has not been set
+      self.to_s.tableize
+    else
+      @table_name
+    end
   end
 
   def self.all
@@ -36,7 +77,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
