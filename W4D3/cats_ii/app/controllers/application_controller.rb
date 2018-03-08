6acff83eba993a710114d054d6bf2088a2f09904
Yangchen Shen <shyangch@gmail.com>
2018-03-07 17:11:48 -0800

    refactored login_user! and bounce user back if trying to create new user/login when already logged in

--- W4D3/cats_ii/app/controllers/application_controller.rb
+++ W4D3/cats_ii/app/controllers/application_controller.rb
@@ -6,4 +6,28 @@ class ApplicationController < ActionController::Base
   def current_user
     User.find_by(session_token: session[:session_token])
   end
+
+  def login_user!
+    user = User.find_by_credentials(user_params[:user_name], user_params[:password])
+
+    if user.nil?
+      flash[:errors] = ["Wrong credentials"]
+      return false
+    else
+      session[:session_token] = user.reset_session_token!
+      return true
+    end
+  end
+
+  def redirect_if_logged_in
+    unless session[:session_token].nil? || session[:session_token].empty?
+      flash[:errors] = ["You are already logged in"]
+      redirect_to cats_url
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
 end
--- W4D3/cats_ii/app/controllers/sessions_controller.rb
+++ W4D3/cats_ii/app/controllers/sessions_controller.rb
@@ -1,16 +1,14 @@
 class SessionsController < ApplicationController
+  before_action :redirect_if_logged_in, only: :new
   def new
     render :new
   end
 
   def create
-    user = User.find_by_credentials(user_params[:user_name], user_params[:password])
-    if user.nil?
-      flash[:errors] = ["Wrong credentials"]
-      render :new
-    else
-      session[:session_token] = user.reset_session_token!
+    if login_user!
       redirect_to cats_url
+    else
+      render :new
     end
   end
 
@@ -18,11 +16,5 @@ class SessionsController < ApplicationController
     current_user.reset_session_token! unless current_user.nil?
     session[:session_token] = ""
     redirect_to cats_url
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:user_name, :password)
   end
 end
