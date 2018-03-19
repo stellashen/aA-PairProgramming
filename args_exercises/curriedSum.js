80b4625eeabb5cb82b425a8287dfbebb53c62137
Yangchen Shen <shyangch@gmail.com>
2018-03-19 15:15:41 -0700

    fixed bugs in curried functions

--- args_exercises/curriedSum.js
+++ args_exercises/curriedSum.js
@@ -1,14 +1,15 @@
 function curriedSum(numArgs) {
   let numbers = [];
-  let args = numArgs;
   const _curriedSum = function (num) {
     numbers.push(num);
-    if (numbers.length === args) {
+    if (numbers.length >= numArgs) {
       let total = 0;
       for (let i = 0; i < numbers.length; i++ ) {
         total += numbers[i];
       }
       return total;
+    } else {
+      return _curriedSum;
     }
   };
 
@@ -20,29 +21,31 @@ function curriedSum(numArgs) {
 // sum(5);
 // sum(5);
 // console.log(sum(5));
-// console.log(sum(5)(30)(20)(1)); // => 56
+// console.log(
+//   sum(5)(30)(20)(1)
+// ); // => 56
 
 Function.prototype.curry = function(numArgs) {
-  let numbers = [];
-  let numrg = numArgs;
-  const sum = function (arg) {
-    numbers.push(arg);
-    if (numbers.length === numrg) {
-      let total = 0;
-      for (let i = 0; i < numbers.length; i++ ) {
-        total += numbers[i];
-      }
-      return total;
+  let args = [];
+  let fn = this;
+  const _curriedFunc = function (arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return fn.apply(null, args);
+    } else {
+      return _curriedFunc;
     }
   };
-  return sum;
+  return _curriedFunc;
 };
 
 function sumThree(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-let f1 = sumThree().curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
 f1 = f1(4); // [Function]
 f1 = f1(20); // [Function]
-f1 = f1(6); // = 30
+console.log(f1 = f1(6)); // = 30
+
+console.log(sumThree.curry(3)(4)(20)(6));
