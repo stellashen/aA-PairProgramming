5401195dcfe654fc5b4f09cdf3d9973090d635d3
Yangchen Shen <shyangch@gmail.com>
2018-03-28 15:33:02 -0700

    Create react component for rendering pokeemon and connect it to the redux store

--- W7D3/pokedex/frontend/pokedex.jsx
+++ W7D3/pokedex/frontend/pokedex.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
+
+//for testing
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon, RECEIVE_ALL_POKEMON, requestAllPokemon } from './actions/pokemon_actions';
 import selectAllPokemon from './reducers/selectors';
@@ -8,7 +11,7 @@ import selectAllPokemon from './reducers/selectors';
 //TODO: remove stuff set on window used for testing
 
 document.addEventListener('DOMContentLoaded', () => {
-  const rootEl = document.getElementById('root');
+  const root = document.getElementById('root');
   const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
@@ -16,5 +19,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.fetchAllPokemon = fetchAllPokemon;
   window.requestAllPokemon = requestAllPokemon;
   window.selectAllPokemon = selectAllPokemon;
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  ReactDOM.render(<Root store={store}/>, root);
 });
--- W7D3/pokedex/frontend/reducers/pokemon_reducer.js
+++ W7D3/pokedex/frontend/reducers/pokemon_reducer.js
@@ -2,6 +2,7 @@ import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
 import { fetchAllPokemon } from '../util/api_util';
 
 const pokemonReducer = (state = {}, action) => {
+  Object.freeze(state);
   switch(action.type) {
     case RECEIVE_ALL_POKEMON:
       return action.pokemon;
